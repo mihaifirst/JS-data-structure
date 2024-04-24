895ce1b42569b285bbc27934144128189e9ad080
mihaifirst <varzaru.alexmihai@gmail.com>
2024-04-24 21:18:08 +0300

    finished 1st section

--- traversy-js-challenges/01-basic-challenges-1/09-count-vowels/count-vowels.js
+++ traversy-js-challenges/01-basic-challenges-1/09-count-vowels/count-vowels.js
@@ -1,3 +1,22 @@
-function countVowels() {}
+function countVowels(str, vowels) {
+  let counter = 0;
+  let name = str.toLowerCase();
+
+  for (let i = 0; i < name.length; i++) {
+    const char = name[i];
+
+    if (
+      char === "a" ||
+      char === "e" ||
+      char === "i" ||
+      char === "o" ||
+      char === "u"
+    ) {
+      counter++;
+    }
+  }
+
+  return counter;
+}
 
 module.exports = countVowels;
